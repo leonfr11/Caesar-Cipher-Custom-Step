436990c1babeda33e9e2084436cf8cc6db138009
Leonidas <111084181+leonfr11@users.noreply.github.com>
2022-08-25 11:39:41 -0400

    Add files via upload

---
+++ decipher.py
@@ -0,0 +1,27 @@
+sub = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+       'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R',
+       'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '!', '@', '#', '$',
+       '%', '^', '&', '*', '(', ')', '-', '+', '_', '=', '/', '.', ',', ' ', '~', '`', ';', ':', '{', '}', '[', ']',
+       '?', '<', '>', '"', '\'', '\\', '|']
+def findorder(character):
+    order=-1
+    for c in sub:
+        if c == character:
+            order = sub.index(c)
+            return order
+
+def replace(order,step):
+    while step-len(sub) > len(sub):
+        step-=len(sub)
+    if order - step < 0:
+        print(sub[len(sub) + order - step], end="")
+    else:
+        print(sub[order - step], end="")
+
+print("Enter the step: ", end="")
+step = input()
+step = int(step)
+print("Enter the message: ", end="")
+msg = input()
+for x in msg:
+    replace(findorder(x),step)
